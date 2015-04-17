b617218230a5871b87644bbbf2b63b226ae571c1
Marcelo <marcelo.avan@gmail.com>
2015-04-16 23:37:47 -0300

    include popcorn-time

--- after-install.sh
+++ after-install.sh
@@ -17,6 +17,7 @@
 
 ### PPA / SOURCES ###
 sudo add-apt-repository -y ppa:webupd8team/java
+sudo add-apt-repository ppa:webupd8team/popcorntime
 sudo add-apt-repository -y ppa:libreoffice/ppa
 if [ -e "/etc/apt/sources.list.d/spotify.list" ]; then
 	echo "source spotify list already exists in '/etc/apt/sources.list.d/spotify.list'"
@@ -35,13 +36,13 @@ sudo apt-get dist-upgrade
 
 ### Download via apt ###
 # l1 - Flash Player
-# l2 - Multimedia codecs
+# l2 - Multimedia and codecs
 # l3 - Rar and other archiving utilities
 # l4 - Infos about hard, system. partitions and process
 # l5 - Editors and programming
 # l6 - build essentials
 sudo apt-get install \
-	pepperflashplugin-nonfree \
+	pepperflashplugin-nonfree popcorn-time \
 	ubuntu-restricted-extras spotify-client \
 	unace p7zip-rar sharutils rar arj lunzip lzip \
 	hardinfo htop iptraf gparted unetbootin whois \
